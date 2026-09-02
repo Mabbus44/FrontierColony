71c47da8c9098868680f2cea7bfe1c9d5eae398b
Mabbus git <rasun44@hotmail.com>
2026-09-02 22:48:52 +0200

    Creating world map tiles work (only concrete)

--- FactorioMod/control.lua
+++ FactorioMod/control.lua
@@ -5,7 +5,18 @@ script.on_init(function()
   log("on_init");
   local WorldMapSurfaceSettings = {
     width = WorldMap.width,
-    height = WorldMap.height
+    height = WorldMap.height,
+		default_enable_all_autoplace_controls = false,
+		autoplace_controls = {}, 
+		autoplace_settings = {
+				["tile"] = {
+						settings = {
+								["out-of-map"] = { frequency = "normal", size = "normal", richness = "normal" }
+						}
+				}
+		},
+		starting_area = "none",
+		cliffiness = 0
   }
 	log("Surfaces before");
 	for name, surface in pairs(game.surfaces) do
@@ -16,30 +27,6 @@ script.on_init(function()
 	for name, surface in pairs(game.surfaces) do
 		log("Surface: " .. name);
 	end
-  local empty_map_gen_settings = {
-      default_enable_all_autoplace_controls = false,
-      autoplace_controls = {}, 
-      autoplace_settings = {
-          ["tile"] = {
-              settings = {
-                  ["out-of-map"] = { frequency = "normal", size = "normal", richness = "normal" }
-              }
-          }
-      },
-      starting_area = "none",
-      cliffiness = 0
-  };
-
-  local worldMapTiles = {}
-  local middleX = math.floor(WorldMap.width / 2);
-  local middleY = math.floor(WorldMap.height / 2);
-  for y, row in pairs(WorldMap.tiles) do
-    for x, tile in ipairs(row) do
-      table.insert(worldMapTiles, {name = "refined-concrete", position = {x - middleX, y - middleY}});
-    end
-  end
-  
-  worldMapSurface.set_tiles(worldMapTiles)
 end)
 
 script.on_event(defines.events.on_player_created, function(event)
@@ -70,4 +57,31 @@ script.on_event(defines.events.on_cutscene_cancelled, function(event)
 		log("Surface: " .. name);
 	end
 
+end)
+
+script.on_event(defines.events.on_chunk_generated, function(event)
+	log("on_chunk_generated " .. event.surface.name .. " " .. event.position.x .. "," .. event.position.y .. " (" .. event.area.left_top.x .. "," .. event.area.left_top.y .. ")-(" .. event.area.right_bottom.x .. "," .. event.area.right_bottom.y .. ")");
+
+  local worldMapSurface = event.surface;
+	if worldMapSurface.name ~= worldMapSurfaceName then
+		return;
+	end
+  local middleX = math.floor(WorldMap.width / 2) + 1;	-- This will generate from -a to +a if odd, and -a to +(a-1) if even. This is the same as the built in generation does
+  local middleY = math.floor(WorldMap.height / 2) + 1;
+	local minX = math.max(1 - middleX, event.area.left_top.x);
+	local minY = math.max(1 - middleY, event.area.left_top.y);
+	local maxX = math.min(WorldMap.width - middleX, event.area.right_bottom.x-1);
+	local maxY = math.min(WorldMap.height - middleY, event.area.right_bottom.y-1);
+  if minX > maxX or minY > maxY then
+		log("Chunk outside map, skipping tile placement");
+		return;
+	end;
+	local worldMapTiles = {};
+	log("Setting (" .. minX .. "," .. minY .. ")-(" .. maxX .. "," .. maxY .. ")");
+	for y = minY, maxY do
+    for x = minX, maxX do
+      table.insert(worldMapTiles, {name = "refined-concrete", position = {x, y}});
+    end
+  end
+  worldMapSurface.set_tiles(worldMapTiles);
 end)

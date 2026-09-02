55aacedfdcbf3e60300abb7034e3631c4a19c59b
Mabbus git <rasun44@hotmail.com>
2026-09-02 16:41:01 +0200

    World map creation tested

--- FactorioMod/control.lua
+++ FactorioMod/control.lua
@@ -1,14 +1,21 @@
 local WorldMap = require("Assets.worldMap")
+local worldMapSurfaceName = "world_map";
 
 script.on_init(function()
-  game.print("This rund when the game startsr");
-  local worldMapSurfaceName = "world_map";
+  log("on_init");
   local WorldMapSurfaceSettings = {
     width = WorldMap.width,
     height = WorldMap.height
   }
+	log("Surfaces before");
+	for name, surface in pairs(game.surfaces) do
+		log("Surface: " .. name);
+	end
   local worldMapSurface = game.create_surface(worldMapSurfaceName, WorldMapSurfaceSettings);
-
+	log("Surfaces after");
+	for name, surface in pairs(game.surfaces) do
+		log("Surface: " .. name);
+	end
   local empty_map_gen_settings = {
       default_enable_all_autoplace_controls = false,
       autoplace_controls = {}, 
@@ -24,18 +31,43 @@ script.on_init(function()
   };
 
   local worldMapTiles = {}
-  local middleX = WorldMap.width // 2;
-  local middleY = WorldMap.height // 2;
+  local middleX = math.floor(WorldMap.width / 2);
+  local middleY = math.floor(WorldMap.height / 2);
   for y, row in pairs(WorldMap.tiles) do
     for x, tile in ipairs(row) do
       table.insert(worldMapTiles, {name = "refined-concrete", position = {x - middleX, y - middleY}});
     end
   end
   
-  worldMapSurface.set_tiles(tiles_to_set)
+  worldMapSurface.set_tiles(worldMapTiles)
+end)
 
-  for _, player in pairs(game.players) do
-    player.teleport({x = 0, y = 0}, worldMapSurfaceName)
-  end
+script.on_event(defines.events.on_player_created, function(event)
+  log("on_player_created");
+end)
+
+script.on_event(defines.events.on_cutscene_started, function(event)
+  log("on_cutscene_started");
+	local player = game.get_player(event.player_index);
+	player.exit_cutscene();
+end)
+
+script.on_event(defines.events.on_cutscene_cancelled, function(event)
+  log("on_cutscene_cancelled");
+	local player = game.get_player(event.player_index);
+	log("player.teleport: " .. tostring(player.teleport({x = 0, y = 0}, worldMapSurfaceName)));
+	local nauvis = game.surfaces["nauvis"];
+	log("Surfaces before");
+	for name, surface in pairs(game.surfaces) do
+		log("Surface: " .. name);
+	end
+	if nauvis then
+		log("nauvis deleted");
+		log("game.delete_surface: " .. tostring(game.delete_surface(nauvis)));
+	end
+	log("Surfaces after");
+	for name, surface in pairs(game.surfaces) do
+		log("Surface: " .. name);
+	end
 
 end)

a0c1c38ee04cda3b6df4e6fbd8ca46753f9ef9c5
Mabbus <rasun44@hotmail.com>
2026-09-06 13:49:20 +0200

    Belt inserter and assambler without power

--- FactorioMod/data.lua
+++ FactorioMod/data.lua
@@ -1,7 +1,20 @@
 local settlement = table.deepcopy(data.raw["container"]["steel-chest"])
-
 settlement.name = "settlement"
 
+local assembler = table.deepcopy(data.raw["assembling-machine"]["assembling-machine-2"])
+assembler.name = "fc-assembler"
+assembler.energy_source = { type = "void" }
+
+local inserter = table.deepcopy(data.raw["inserter"]["fast-inserter"])
+inserter.name = "fc-inserter"
+inserter.energy_source = { type = "void" }
+
+local belt = table.deepcopy(data.raw["transport-belt"]["fast-transport-belt"])
+belt.name = "fc-belt"
+
 data:extend({
-  settlement
+  settlement,
+  assembler,
+  inserter,
+  belt
 })

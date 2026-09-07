23d4bc48a9bdc3c94e8525d2c0dc03c137714940
Mabbus <rasun44@hotmail.com>
2026-09-07 09:17:09 +0200

    blacklist data

--- FactorioMod/data.lua
+++ FactorioMod/data.lua
@@ -60,28 +60,68 @@ stoneBeltRecipe.results = {{ type = "item", name = "stone-belt", amount = 1 }}
 stoneBeltRecipe.ingredients = {{ amount = 1, name = "stone", type = "item" }}
 
 local removeRecipes = {
-	"wooden-chest"
+	-- Logistics
+	"wooden-chest", "iron-chest", "steel-chest", "storage-tank",
+	"transport-belt", "fast-transport-belt", "express-transport-belt", "turbo-transport-belt",
+	"underground-belt", "fast-underground-belt", "express-underground-belt", "turbo-underground-belt",
+	"splitter", "fast-splitter", "express-splitter", "turbo-splitter",
+	"burner-inserter", "inserter", "long-handed-inserter", "fast-inserter", "bulk-inserter", "stack-inserter",
+	"small-electric-pole", "medium-electric-pole", "big-electric-pole", "substation", "pipe", "pipe-to-ground", "pump",
+	"rail", "rail-ramp", "rail-support", "train-stop", "rail-signal", "rail-chain-signal", "locomotive", "cargo-wagon", "fluid-wagon", "artillery-wagon",
+	"car", "tank", "spidertron",
+	"logistic-robot", "construction-robot", "active-provider-chest", "passive-provider-chest", "storage-chest", "buffer-chest", "requester-chest", "robobort",
+	"lamp", "aritmetic-combinator", "decider-combinator", "selector-combinator", "constant-combinator", "power-switch", "programmable-speaker", "display-panel",
+	"stone-brick", "concrete", "hazard-concrete", "refined-concrete", "refined-hazard-concrete", "landfill", "artificial-yumako-soil", "overgrowth-yumako-soil", "artificial-jellynut-soil", "overgrowth-jellynut-soil", "ice-platform", "foundation", "cliff-explosives",
+	-- Production
+	"repair-pack",
+	"boiler", "steam-engine", "solar-panel", "accumulator", "nuclear-reactor", "heat-pipe", "heat-exchanger", "steam-turbine", "fusion-reactor", "fusion-generator",
+	"burner-mining-drill", "electric-mining-drill", "big electric-mining-drill", "offshore-pump", "pumpjack",
+	"stone-furnace", "steel-furnace", "electric-furnace", "foundry", "recycler",
+	"agricultural-tower", "biochamber",
+	"assembling-machine-1", "assembling-machine-2", "assembling-machine-3", "oil-refinery", "chemical-plant", "centrifuge", "electromagnetic-plant", "cryogenic-plant", "lab", "biolab",
+	"lightning-rod", "lightning-collector", "heating-tower",
+	"beacon", "speed-module", "speed-module-2", "speed-module-3", "effectivity-module", "effectivity-module-2", "effectivity-module-3", "productivity-module", "productivity-module-2", "productivity-module-3", "quality-module", "quality-module-2", "quality-module-3",
+	-- Intermediate
+	"iron-plate", "copper-plate", "steel-plate", "plastic-bar", "sulfur", "battery", "explosives", "carbon",
 }
-local removedItems = {}
+local removeItems = {
+	-- Logistics
+	"spidertron-remote",
+	"red-wire", "green-wire",
+	-- Production
+	"blueprint", "deconstruction-planner", "upgrade-planner", "blueprint-book",""
+	"captive-biter-spawner",
+	-- Intermediate
+	"solid-fuel",
+	"wood", "coal", "stone", "iron-ore", "copper-ore", "uranium-ore", "raw-fish", "ice",
+
+}
 for _,recipeName in ipairs(removeRecipes) do
-	removedItems[data.raw.recipe[recipeName]["results"][1].name] = true
-	log("recipe result: " .. data.raw.recipe[recipeName]["results"][1].name)
-	data.raw.recipe[recipeName] = nil
-end
-
-local count = 0
-for _ in pairs(removedItems) do count = count + 1 end
-log("list len: " .. count)
-for removedItemName, _ in pairs(removedItems) do
-	log("removedItemName: " .. removedItemName)
-	data.raw.item[removedItemName] = nil
-end
-
-for prototypeGroupName, prototypes in pairs(data.raw) do
-	for prototypeName, prototype in pairs(prototypes) do
-		if prototype.minable and removedItems[prototype.minable.result] then
-			prototype.minable = nil
+	if data.raw.recipe[recipeName] == nil then
+		log("error: recipe " .. recipeName .. " does not exist" )
+	else
+		if data.raw.recipe[recipeName]["results"] == nil then
+			log("error: recipe " .. recipeName .. " has no results")
+		else
+			if data.raw.recipe[recipeName]["results"][2] ~= nil then
+				log("warning: recipe " .. recipeName .. " has more than one result")
+			end
+			table.insert(removeItems, data.raw.recipe[recipeName]["results"][1].name)
+			log("removed recipe " .. recipeName)
+			log("added " .. data.raw.recipe[recipeName]["results"][1].name .. " to removeItems")
+			data.raw.recipe[recipeName].hidden = true
+			data.raw.recipe[recipeName].hidden_in_factoriopedia = true
 		end
+	end
+end
+
+for _, removedItemName in ipairs(removeItems) do
+	if data.raw.item[removedItemName] == nil then
+		log("error: item " .. removedItemName .. " does not exist")
+	else
+		log("removed item " .. removedItemName)
+		data.raw.item[removedItemName].hidden = true
+		data.raw.item[removedItemName].hidden_in_factoriopedia = true
 	end
 end
 
